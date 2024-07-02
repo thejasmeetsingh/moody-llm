b8eff97a5c74300d106089d9d57617c80619dcb4
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-07-02 16:36:27 +0530

    Integrated langchain and ollama

--- llm.py
+++ llm.py
@@ -1,6 +1,24 @@
-from lifespan import tokenizer, model
 import random
 
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+
+llm = ChatOllama(model="llama3")
+
+output_parser = StrOutputParser()
+
+prompt = ChatPromptTemplate.from_messages([
+    (
+        "system",
+        "You are a person whose mood keeps changing. Give a response based on your mood, which is {mood}."
+    ),
+    MessagesPlaceholder(variable_name="messages")
+])
+
+chain = prompt | llm | output_parser
 
 moods: dict[int, str] = {
     1: "Angry",
@@ -10,55 +28,34 @@ moods: dict[int, str] = {
     5: "Fearful",
 }
 
-prompt: str = "Suppose you are in {} mood, Give the response based on your mood."
 
 def templatize_message_history(history: list) -> list:
-    messages: list[dict[str, str]] = []
+    messages: list[HumanMessage | AIMessage] = []
 
     for _history in history:
         messages.extend([
-            {"role": "system", "content": _history["system"]["message"]},
-            {"role": "user", "content": _history["user"]["message"]},
+            HumanMessage(content=_history["user"]["message"]),
+            AIMessage(content=_history["system"]["message"])
         ])
 
     return messages
 
+
 def generate_random_mood() -> int:
     return random.randrange(1, len(moods) + 1)
 
+
 def get_llm_response(history: list, _user_message: str) -> dict[str, str]:
-    messages: list[dict[str, str]] = templatize_message_history(history)
-    mood: str = moods[generate_random_mood()]
+    messages: list = templatize_message_history(history)
+    key: int = generate_random_mood()
 
-    messages.extend([
-        {"role": "system", "content": prompt.format(mood)},
-        {"role": "user", "content": _user_message}
-    ])
+    messages.append(HumanMessage(content=_user_message))
 
-    input_ids = tokenizer.apply_chat_template(
-        messages,
-        add_generation_prompt=True,
-        return_tensors="pt"
-    ).to(model.device)
-
-    terminators = [
-        tokenizer.eos_token_id,
-        tokenizer.convert_tokens_to_ids("<|eot_id|>")
-    ]
-
-    outputs = model.generate(
-        input_ids,
-        max_new_tokens=256,
-        eos_token_id=terminators,
-        do_sample=True,
-        temperature=0.6,
-        top_p=0.9,
+    response: str = chain.invoke(
+        {"messages": messages, "mood": moods[key]}
     )
 
-    response = outputs[0][input_ids.shape[-1]:]
-    message = tokenizer.decode(response, skip_special_tokens=True)
-
     return {
-        "message": message,
-        "mood": mood
+        "message": response,
+        "mood": key
     }
