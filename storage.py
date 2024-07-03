ecd8e37115e3cf0c82e8d329f2557d28a0ea0c6d
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-07-03 21:35:10 +0530

    Added storage class for interacting with supabase

--- storage.py
+++ storage.py
@@ -1,35 +1,23 @@
-import os
 import uuid
 
-from supabase import AClient, acreate_client
+from supabase import AClient
 
 
-table = os.getenv("SUPABASE_TABLE_NAME")
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+class Storage:
+    def __init__(self, client: AClient, table: str) -> None:
+        self.table = table
+        self.client = client
+    
+    async def get_messages(self, user_id: uuid.UUID, limit: int | None = None):
+        query = self.client.table(self.table).select("message").eq("user_id", str(user_id))
+        if limit is not None:
+            query = query.limit(limit)
 
-if not table or not url or not key:
-    raise EnvironmentError("Supabase credentials are not provided.")
+        response = await query.order("created_at", desc=True).execute()
+        return response.data
 
-
-async def get_client():
-    client = await acreate_client(url, key)
-    try:
-        yield client
-    except Exception as _:
-        pass
-
-
-async def get_messages(client: AClient, user_id: uuid.UUID, limit: int | None = None) -> dict[str, list]:
-    query = client.table(table).select("message").eq("user_id", user_id)
-    if limit is not None:
-        query = query.limit(limit)
-
-    return await query.order("created_at", desc=True).execute()
-
-
-async def add_message(client: AClient, user_id: uuid.UUID, message: dict) -> None:
-    await client.table(table).insert({
-        "user_id": user_id,
-        "message": message
-    }).execute()
+    async def add_message(self, user_id: uuid.UUID, message: dict):
+        await self.client.table(self.table).insert({
+            "user_id": str(user_id),
+            "message": message
+        }).execute()
