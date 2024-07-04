f65b27a9ee0b7d821bf510312a9da1ecc14af910
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-07-04 16:49:37 +0530

    re-strucutre the project and initialize frontend

--- Makefile
+++
@@ -1,4 +0,0 @@
-up:
-	docker run --name redis -p 6379:6379 -d redis:7.2.3-alpine
-
-	gunicorn main:app
--- src/backend/storage.py
+++ src/backend/storage.py
@@ -7,9 +7,12 @@ class Storage:
     def __init__(self, client: AClient, table: str) -> None:
         self.table = table
         self.client = client
-    
+
     async def get_messages(self, user_id: uuid.UUID, limit: int | None = None):
-        query = self.client.table(self.table).select("message").eq("user_id", str(user_id))
+        query = self.client.table(self.table).\
+            select("message").\
+            eq("user_id", str(user_id))
+
         if limit is not None:
             query = query.limit(limit)
 
