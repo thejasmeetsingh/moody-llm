f35e50ff1c9d3556ed58ab4a8d1dd7c1cd822398
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-07-05 11:32:29 +0530

    Removed streaming parser

--- src/backend/llm.py
+++ src/backend/llm.py
@@ -1,20 +1,14 @@
 import random
-from typing import Iterable
 
 from langchain_community.chat_models import ChatOllama
-from langchain_core.messages import AIMessage, AIMessageChunk, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnableGenerator
 
 from utils import markdown_to_html
 
 
 llm = ChatOllama(model="llama3")
-
-
-def custom_output_parser(chunks: Iterable[AIMessageChunk]) -> Iterable[str]:
-    for chunk in chunks:
-        yield markdown_to_html(chunk.content)
 
 
 prompt = ChatPromptTemplate.from_messages([
@@ -25,7 +19,7 @@ prompt = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name="messages")
 ])
 
-chain = prompt | llm | RunnableGenerator(custom_output_parser)
+chain = prompt | llm | StrOutputParser()
 
 moods: dict[int, str] = {
     1: "Angry",
@@ -57,14 +51,12 @@ async def get_llm_response(history: list, _user_message: str) -> dict[str, str]:
     key: int = generate_random_mood()
 
     messages.append(HumanMessage(content=_user_message))
-    response: str = ""
 
-    for chunk in chain.stream(
+    response: str = chain.invoke(
         {"messages": messages, "mood": moods[key]}
-    ):
-        response += chunk
+    )
 
     return {
-        "message": response,
+        "message": markdown_to_html(response),
         "mood": key
     }
