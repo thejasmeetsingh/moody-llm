4af254e582652d613161ea99672cc2c1af151c96
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-07-06 22:01:51 +0530

    Updated display messages componenets because AAPI response strucutre

--- src/frontend/src/components/DisplayReceivedMessage.jsx
+++ src/frontend/src/components/DisplayReceivedMessage.jsx
@@ -1,7 +1,7 @@
 import DOMPurify from "dompurify";
 
 export default function DisplaySentMessage({ message }) {
-  const sanitizedHTML = DOMPurify.sanitize(message.message);
+  const sanitizedHTML = DOMPurify.sanitize(message.content);
 
   return (
     <div className="mb-4 self-center p-4 max-w-xs">
