141db9d93e2c820d87272cd802de936926713dd1
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-07-07 01:46:22 +0530

    Updated SendMessage Form styling

--- src/frontend/src/components/SendMessage.jsx
+++ src/frontend/src/components/SendMessage.jsx
@@ -1,32 +1,51 @@
 import { useState } from "react";
+import useMessagesContext from "../hooks/use-messages-context";
 
 export default function SendMessage() {
   const [message, setMessage] = useState("");
+  const [inputDisabled, setInputDisabled] = useState(false);
+  const { sendMessage } = useMessagesContext();
 
   const onFormSubmit = (e) => {
     e.preventDefault();
-    console.log(message);
+    setInputDisabled(true);
+
+    setTimeout(async () => {
+      await sendMessage(message);
+      setMessage("");
+      setInputDisabled(false);
+    });
   };
 
   return (
     <div>
       <form
         onSubmit={onFormSubmit}
-        className="flex p-4 border-t border-gray-300 bg-white"
+        className="flex flex-col p-4 border-t border-gray-300"
       >
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          placeholder="Type message here..."
-          className="flex-1 p-4 border border-gray-300 drop-shadow-lg shadow-grey-500 rounded-lg mr-4"
-        />
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white shadow-md shadow-blue-500 rounded-lg"
-        >
-          Send
-        </button>
+        <div className="flex items-center">
+          <div className="relative w-full">
+            <input
+              type="text"
+              value={message}
+              maxLength={2000}
+              disabled={inputDisabled}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Type message here..."
+              className="w-full p-4 border border-gray-300 drop-shadow-lg shadow-grey-500 rounded-lg"
+            />
+            <div className="absolute right-2 bottom-2 text-xs text-gray-400">
+              {message.length}/2000
+            </div>
+          </div>
+          <button
+            type="submit"
+            disabled={inputDisabled}
+            className="ml-4 p-4 bg-blue-500 text-white shadow-md shadow-blue-500 rounded-lg"
+          >
+            Send
+          </button>
+        </div>
       </form>
     </div>
   );
--- src/frontend/src/components/SentMessage.jsx
+++ src/frontend/src/components/SentMessage.jsx
@@ -3,7 +3,7 @@ import Timestamp from "./Timestamp";
 export default function SentMessage({ content, timestamp }) {
   return (
     <div className="mb-6 flex justify-end">
-      <div className="max-w-xs">
+      <div className="max-w-xs break-words">
         <div className="p-4 rounded-lg bg-blue-200 text-sm">{content}</div>
         <div className="text-right">
           <Timestamp timestamp={timestamp} />
