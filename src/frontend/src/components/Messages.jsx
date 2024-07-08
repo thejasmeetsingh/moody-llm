e490ccb3f00f4075bb45ce1aa80b5c231289ab50
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2024-07-08 20:57:38 +0530

    Updated tailwind config for responsiveness

--- src/frontend/src/components/Messages.jsx
+++ src/frontend/src/components/Messages.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useEffect } from "react";
 import SentMessage from "./SentMessage";
 import ReceivedMessage from "./ReceivedMessage";
 import useMessagesContext from "../hooks/use-messages-context";
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      tablet: "640px",
+
+      laptop: "1024px",
+
+      desktop: "1280px",
+    },
   },
   plugins: [],
 };
